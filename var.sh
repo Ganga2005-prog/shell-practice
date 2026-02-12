302873fcc049007806cb123f45903f32bcf6a7ae
Ganga2005-prog <gangagangak99@gmail.com>
2026-02-12 17:39:18 +0530

    pushh

--- var.sh
+++ var.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
-
-echo " script executed at: $TIMESTAMP"
+echo "all args passed to scripts: $@"
+echo "number of variables passed to script: $#"
+echo "script name: $0"
+echo "present directory: $PWD"
+echo "who is running: $ USER"
+echo "home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "PID of recently executed background processs: #!"
+echo "all args passed to scripts: $*"
