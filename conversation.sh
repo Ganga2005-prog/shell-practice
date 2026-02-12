f42f4be34871937e2a50f5cc457fc7b18fd46574
Ganga2005-prog <gangagangak99@gmail.com>
2026-02-12 17:00:15 +0530

    pushh

--- conversation.sh
+++ conversation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo "suresh:hello"
-echo "ramesh:hello,how are you"
-echo "suresh:i'm fine"
+PERSON1=suresh
+PERSON2=ramesh
+echo "$PERSON1:hello"
+echo "$PERSON2:hello,how are you"
+echo "$PERSON2:i'm fine"
+echo "$PERSON2:i'm also fine"
