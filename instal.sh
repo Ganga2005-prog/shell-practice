d6018cd71ba89f2b533b0755e469911b74751793
Ganga2005-prog <gangagangak99@gmail.com>
2026-02-13 09:12:32 +0530

    push

--- instal.sh
+++ instal.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
 
 if [ $USERID -ne 0 ]; then
     echo "run with root user access"
     exit 1
 fi 
+
+mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
@@ -17,14 +22,14 @@ VALIDATE(){
 }
 
 
-dnf install nginx -y
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "nginx installation"
 
-dnf install mysql -y
+dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "mysql installation"
 
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "nodejs installation"
 
 
